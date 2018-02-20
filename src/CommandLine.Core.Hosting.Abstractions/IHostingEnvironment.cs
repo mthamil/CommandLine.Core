2a6cb192fd17f5b603329690a08eff7985228198
Matthew Hamilton <matthamilton@live.com>
2018-02-19 20:06:11 -0600

    Add working directory to hosting environment.

--- src/CommandLine.Core.Hosting.Abstractions/IHostingEnvironment.cs
+++ src/CommandLine.Core.Hosting.Abstractions/IHostingEnvironment.cs
@@ -1,9 +1,15 @@
-﻿namespace CommandLine.Core.Hosting.Abstractions
+﻿using Microsoft.Extensions.FileProviders;
+
+namespace CommandLine.Core.Hosting.Abstractions
 {
     public interface IHostingEnvironment
     {
         string ApplicationName { get; }
 
         string EnvironmentName { get; }
+
+        string WorkingDirectory { get; }
+
+        IFileProvider WorkingDirectoryFileProvider { get; }
     }
 }
--- src/CommandLine.Core.Hosting/HostingEnvironment.cs
+++ src/CommandLine.Core.Hosting/HostingEnvironment.cs
@@ -1,5 +1,7 @@
 ﻿using CommandLine.Core.Hosting.Abstractions;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.FileProviders;
+using System.IO;
 
 namespace CommandLine.Core.Hosting
 {
@@ -9,10 +11,16 @@ namespace CommandLine.Core.Hosting
         {
             ApplicationName = config[HostDefaults.ApplicationNameKey];
             EnvironmentName = config[HostDefaults.EnvironmentNameKey] ?? EnvironmentName;
+            WorkingDirectory = config[HostDefaults.WorkingDirectoryKey] ?? Directory.GetCurrentDirectory();
+            WorkingDirectoryFileProvider = new PhysicalFileProvider(WorkingDirectory);
         }
 
         public string ApplicationName { get; set; }
 
         public string EnvironmentName { get; set; } = "Development";
+
+        public string WorkingDirectory { get; set; }
+
+        public IFileProvider WorkingDirectoryFileProvider { get; set; }
     }
 }
