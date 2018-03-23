db578481d4334b38e6cc32c082d28f584a291927
Matthew Hamilton <matthamilton@live.com>
2018-03-23 01:05:48 -0500

    Add command registration helpers.

---
+++ src/CommandLine.Core.Hosting.CommandLineUtils/RegistrationExtensions.cs
@@ -0,0 +1,20 @@
+﻿using McMaster.Extensions.CommandLineUtils;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CommandLine.Core.Hosting.CommandLineUtils
+{
+    public static class RegistrationExtensions
+    {
+        /// <summary>
+        /// Adds a root level command.
+        /// </summary>
+        public static IServiceCollection AddBaseCommand<TCommand>(this IServiceCollection services) where TCommand : CommandLineApplication => 
+            services.AddScoped<CommandLineApplication, TCommand>();
+
+        /// <summary>
+        /// Adds a child command.
+        /// </summary>
+        public static IServiceCollection AddChildCommand<TCommand>(this IServiceCollection services) where TCommand : CommandLineApplication =>
+            services.AddScoped<TCommand>();
+    }
+}
