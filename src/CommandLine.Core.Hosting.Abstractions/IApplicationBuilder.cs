7d83210e1975fba43f8a80f990377a3a73894ec4
Matthew Hamilton <matthamilton@live.com>
2018-04-09 00:23:57 -0500

    Add more documentation.

--- src/CommandLine.Core.Hosting.Abstractions/IApplicationBuilder.cs
+++ src/CommandLine.Core.Hosting.Abstractions/IApplicationBuilder.cs
@@ -2,12 +2,24 @@
 
 namespace CommandLine.Core.Hosting.Abstractions
 {
+    /// <summary>
+    /// Enables configuration of an application.
+    /// </summary>
     public interface IApplicationBuilder
     {
+        /// <summary>
+        /// The service provider for an application.
+        /// </summary>
         IServiceProvider ApplicationServices { get; }
 
+        /// <summary>
+        /// Uses a delegate to set the core application.
+        /// </summary>
         IApplicationBuilder Use(ApplicationDelegate app);
 
+        /// <summary>
+        /// Builds an application.
+        /// </summary>
         ApplicationDelegate Build();
     }
 }
